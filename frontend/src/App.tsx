260179abfe7bb638f47b9991639b228c5583af00
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-11-17 21:33:18 -0300

    crindo primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
